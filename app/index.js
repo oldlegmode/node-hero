87d37ce16be99b6a9f312073ef1eb389521661f2
oldlegmode <aleh.filkin@consultant.turkcell.com.tr>
2020-02-17 16:00:17 +0300

    Server on express and simple html template

--- app/index.js
+++ app/index.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-const port = 3000;
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.listen(3000);
 
 app.get('/', (request, response) => {
-    response.send('Hello from Express!');
-});
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad hapened', err)
-    }
-
-    console.log(`server is listening on ${port}`);
+    response.render('home', {
+        name: 'John'
+    })
 });
