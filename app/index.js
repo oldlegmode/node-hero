d189aa36f994789bc42838d29ce6de0ba95d143c
oldlegmode <aleh.filkin@consultant.turkcell.com.tr>
2020-02-17 15:39:39 +0300

    Added express, prepare to send HTML pages

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3000;
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    response.end('Hello Node.js Server!')
-};
-const server = http.createServer(requestHandler);
-server.listen(port, (err) => {
+
+app.get('/', (request, response) => {
+    response.send('Hello from Express!');
+});
+
+app.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('something bad hapened', err)
     }
-    console.log(`server is listening on ${port}`)
+
+    console.log(`server is listening on ${port}`);
 });
